6525fbdd46b4bc1a2ba091cb07a2ffe1ed7f3e0b
Dongfang (Tian) Mi <tianm@mit.edu>
2014-08-30 00:39:23 -0400

    first commit :x

---
+++ main.js
@@ -0,0 +1,91 @@
+
+
+$(window).ready(function() {
+
+})
+
+
+
+
+
+/*
+this is what javascript functions look like
+var functionName = function(param, param) {
+	
+}
+*/
+
+
+
+
+
+
+/*
+
+paths to magic fish (if possible)
+nth generation breedable (select number of generations? up to 10)
+
+fish stats
+- by breed
+- by color
+
+
+
+hornet arrowfish
+silky fatfish
+flagged beta
+silky fruitfish
+speckled fruitfish
+canary fruitfish
+stubby comet
+wasp arrowfish
+snubbed fruitfish
+wasp goldshark
+golden arrowfish
+pink spotanus
+pink shark
+wasp goldshark
+hornet arrowfish
+wasp stickfish
+snubbed firearrow
+snubbed goldbulb
+orange leaffish
+hornet arrowfish
+orange beta
+canary fruitfish
+orange leaffish
+speckled goldshark
+pink beta
+hornet fatfish
+golden arrowfish
+pink leaffish
+orange stickfish
+wasp bananafish
+hornet leaffish
+fanned leaffish
+wasp leaffish
+snubbed grouper
+pink firearrow
+canary comet
+hornet comet
+pink fruitfish
+golden bananafish
+wasp bananafish
+speckled bananafish
+orange goldbulb
+hornet fatfish
+golden arrowfish
+golden stickfish
+canary firearrow
+flagged beta
+pink goldbulb
+hornet arrowfish
+hornet fatfish
+orange shark
+silky fatfish
+pink grouper
+hornet arrowfish
+flagged beta
+*/
+
+
